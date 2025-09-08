72227cddf209fdd4d2ae731bbdc0319715f099b9
talhanust <38860520+talhanust@users.noreply.github.com>
2025-09-08 23:36:32 +0500

    Create ExecutiveReports.js

---
+++ src/components/ExecutiveReports.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const ExecutiveReports = () => {
+  return (
+    <Box p={3}>
+      <Typography variant="h4">Executive Reports</Typography>
+      <Typography>Executive reports features will be implemented soon.</Typography>
+    </Box>
+  );
+};
+
+export default ExecutiveReports;
