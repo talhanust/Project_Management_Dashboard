1c9646273609bebbb80bd3c5474072484321b5c5
talhanust <38860520+talhanust@users.noreply.github.com>
2025-09-08 23:35:26 +0500

    Create AIInsights.js

---
+++ src/components/AIInsights.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const AIInsights = () => {
+  return (
+    <Box p={3}>
+      <Typography variant="h4">AI Insights</Typography>
+      <Typography>AI insights features will be implemented soon.</Typography>
+    </Box>
+  );
+};
+
+export default AIInsights;
