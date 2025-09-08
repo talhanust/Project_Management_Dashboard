80d7ce98298b6a20879b8ee65644d1a1b2758bbb
talhanust <38860520+talhanust@users.noreply.github.com>
2025-09-08 23:37:24 +0500

    Create Settings.js

---
+++ src/components/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Settings = () => {
+  return (
+    <Box p={3}>
+      <Typography variant="h4">Settings</Typography>
+      <Typography>Settings features will be implemented soon.</Typography>
+    </Box>
+  );
+};
+
+export default Settings;
